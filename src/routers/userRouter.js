8ef1bb9d430d98d911e7bf8e0107df1a936b1c18
SUNGMIN LEE <sungmin.lee0826@gmail.com>
2021-11-16 15:02:13 +0900

    upload videro/profile pic

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -10,9 +10,9 @@ import {
     postChangePassword
 } from '../controllers/userController';
 import { 
+    avatarUpload,
     protectorMiddleware, 
     publicOnlyMiddleware,
-    uploadFiles
 } from '../middlewares';
 
 const userRouter = express.Router();
@@ -22,13 +22,14 @@ userRouter
 .route('/edit')
 .all(protectorMiddleware)
 .get(getEdit)
-.post(uploadFiles.single("avatar"), postEdit);
+.post(avatarUpload.single("avatar"), postEdit);
 //multer middleware 이 avatar 이라는 이름으로 파일을 받아서 uploads 폴더에 저장후 그 정보를 postEdit에 저장
 // 중요 -- 위의 middleware가 req.files로 return. req.body와 같은 맥락 but input files
 //single 은 한번에 하나의 파일만 받는다는 의미
 userRouter.route('/change-password').get(getChangePassword).post(postChangePassword)
 userRouter.get('/github/start',publicOnlyMiddleware, startGithubLogin);
 userRouter.get('/github/finish',publicOnlyMiddleware, finishGithubLogin);
-userRouter.get(":id", see);
+
+userRouter.get("/:id", see);
 
 export default userRouter;
