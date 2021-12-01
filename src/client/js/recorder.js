4a0142dbf26577be8bb3c5686f7a044f655466d8
SUNGMIN LEE <sungmin.lee0826@gmail.com>
2021-12-01 09:11:59 +0900

    Enable Recording
    Connect webcam and enable it to record on my website by putting the value of
    navigator.mediaDevices.getUserMedia
    to video.srcObject.
    srcObject is used when mediaStream

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,9 +1,11 @@
-import regeneratorRuntime from "regenerator-runtime";
 const startBt = document.querySelector('.startBtn');
+const video = document.querySelector('.preview')
 
 const handleStart = async () => {
-   const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true }); 
-   console.log(stream)
+   const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true, width: 200, height: 200 }); 
+   video.srcObject = stream
+   console.log(video);
+   video.play()
 }
 
 startBt.addEventListener('click', handleStart)
