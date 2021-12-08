0839bb3d06f13952ec24b6e96831284234af9263
SUNGMIN LEE <sungmin.lee0826@gmail.com>
2021-12-08 19:15:00 +0900

    PORT changed to 5000 for testing purpose

--- src/init.js
+++ src/init.js
@@ -1,11 +1,15 @@
 import "regenerator-runtime"
 import 'dotenv/config'
 //.env file 읽고 데이타 가져올 수 있게 해줌 
+import "./models/Video";
+import "./models/User";
+import "./models/Comment"; 
 import "./db"
 import app from "./server"
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const handleListening = () => console.log(`✅Server Listening on port ${PORT}🚀`)
 
-app.listen(4000, handleListening)
+app.listen(5000, handleListening)
+ 
