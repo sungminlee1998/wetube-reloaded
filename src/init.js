cd037c9de378276e7ffc730b8ac24c4de80924ca
SUNGMIN LEE <sungmin.lee0826@gmail.com>
2021-12-08 19:57:59 +0900

    Update init.js

--- src/init.js
+++ src/init.js
@@ -1,13 +1,13 @@
 import "regenerator-runtime"
 import 'dotenv/config'
 //.env file 읽고 데이타 가져올 수 있게 해줌 
+import "./db"
 import "./models/Video";
 import "./models/User";
 import "./models/Comment"; 
-import "./db"
 import app from "./server"
 
-const PORT = process.env.PORT || 5000;
+let PORT = (process.env.PORT || 5000);
 
 const handleListening = () => console.log(`✅Server Listening on port ${PORT}🚀`)
 
